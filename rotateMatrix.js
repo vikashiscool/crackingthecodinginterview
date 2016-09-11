e87b3b6b471faac9d8cd8ffeb6f279db87c7bf11
Marcel Degas <marcel.degas@gmail.com>
2016-09-11 13:39:17 -0700

    added a new problem

---
+++ rotateMatrix.js
@@ -0,0 +1,24 @@
+//Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
+//write a method to rotate the image by 90 degrees Can you do this in place?
+
+function rotateMatrix (matrix) {
+//iterate through the first x axis of the matrix.  Save those values to a new
+//matrix, each value pushed into an indy array.
+
+//iterate through the second x axis of the matrix, push those values into each of
+// those x values (arrays) of the new array
+
+//Return the new matrix.
+}
+
+
+
+
+var practiceMatrixEasy = 	[[0, 0], 
+							[1, 1]];
+
+var practiceMatrixMid =   	[[0, 0, 0], 
+							[1, 1, 1], 
+							[2, 2, 2]];
+
+rotatMatrix(practiceMatrixMid);
